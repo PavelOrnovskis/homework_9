0a39b79d0031a419ebbca816a280970e34abb81f
PavelOrnovskis <pavel.ornovskis@gmail.com>
2025-01-30 01:07:33 +0200

    New test and workflow login

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -8,19 +8,16 @@ test.describe('Login tests', async () => {
       data: LoginDTO.createLoginWithCorrectData(),
     })
     console.log(await response.text())
-    expect(/^eyJhb[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/.test(await response.text())).toBeTruthy()
+    expect(
+      /^eyJhb[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/.test(await response.text()),
+    ).toBeTruthy()
     expect(response.status()).toBe(StatusCodes.OK)
-
-
-
   })
-  test("Negative testing, wrong method to get 405 error (GET method)", async ({request} ) => {
+  test('Negative testing, wrong method to get 405 error (GET method)', async ({ request }) => {
     const response = await request.get(`https://backend.tallinn-learning.ee/login/student`, {
       data: LoginDTO.createLoginWithCorrectData(),
     })
     console.log(await response.text())
     expect(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED)
-
   })
 })
-
