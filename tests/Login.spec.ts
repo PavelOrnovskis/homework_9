71cda3a4f0894456d04e4d6cb89c2f45b414fca6
PavelOrnovskis <pavel.ornovskis@gmail.com>
2025-02-03 01:15:28 +0200

    New test and workflow login

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -1,37 +1,90 @@
 import { expect, test } from '@playwright/test'
 import { LoginDTO } from './DTO/LoginDTO'
 import { StatusCodes } from 'http-status-codes'
+import { OrderDto } from './DTO/OrderDto'
 
 test.describe('Login tests', async () => {
-  test('Successful authorization', async ({ request }) => {
+  test('TL-12-1 Successful authorization', async ({ request }) => {
     const response = await request.post(`https://backend.tallinn-learning.ee/login/student`, {
       data: LoginDTO.createLoginWithCorrectData(),
     })
+
     console.log(await response.text())
-    expect(
-      /^eyJhb[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/.test(await response.text()),
-    ).toBeTruthy()
     expect(response.status()).toBe(StatusCodes.OK)
   })
-  test('Negative testing, wrong method to get 405 error (GET method)', async ({ request }) => {
-    const response = await request.get(`https://backend.tallinn-learning.ee/login/student`, {
+
+  test('TL-12-2 Successful authorization and order creation', async ({ request }) => {
+    const responseLogin = await request.post(`https://backend.tallinn-learning.ee/login/student`, {
       data: LoginDTO.createLoginWithCorrectData(),
     })
-    console.log(await response.text())
-    expect(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED)
+    expect(responseLogin.status()).toBe(StatusCodes.OK);
+    console.log(await responseLogin.text());
+
+    const responseCreateOrder = await request.post(`https://backend.tallinn-learning.ee/orders`, {
+      data: OrderDto.generateRandomOrderDto(),
+      headers: {
+        Authorization: "Bearer " + await responseLogin.text()
+      }
+    })
+    console.log(await responseCreateOrder.text());
+    expect(responseCreateOrder.status()).toBe(StatusCodes.OK);
   })
-  test('Negative testing, wrong method to get 405 error (DELETE method)', async ({ request }) => {
-    const response = await request.delete(`https://backend.tallinn-learning.ee/login/student`, {
+
+  test('TL-12-3 Successful authorization, order creation and order status', async ({ request }) => {
+    const responseLogin = await request.post(`https://backend.tallinn-learning.ee/login/student`, {
       data: LoginDTO.createLoginWithCorrectData(),
     })
-    console.log(await response.text())
-    expect(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED)
+    expect(responseLogin.status()).toBe(StatusCodes.OK);
+
+    const responseCreateOrder = await request.post(`https://backend.tallinn-learning.ee/orders`, {
+      data: OrderDto.generateRandomOrderDto(),
+      headers: {
+        Authorization: "Bearer " + await responseLogin.text()
+      }
+    })
+    expect(responseCreateOrder.status()).toBe(StatusCodes.OK);
+    const createdOrder = OrderDto.serializeResponse(await responseCreateOrder.json())
+    expect(createdOrder.id).toBeDefined();
+    expect(createdOrder.id).toBeGreaterThan(0);
+
+    const responseOrderStatus = await request.get(`https://backend.tallinn-learning.ee/orders/${createdOrder.id}`, {
+      headers: {
+        Authorization: "Bearer " + await responseLogin.text()
+      }
+    })
+    expect(responseOrderStatus.status()).toBe(StatusCodes.OK);
+    const requestedOrder = OrderDto.serializeResponse(await responseOrderStatus.json());
+    expect(requestedOrder.status).toBeDefined();
+    expect(requestedOrder.status).toBe("OPEN");
+    console.log(await responseCreateOrder.text());
   })
-  test('Negative testing, wrong method to get 405 error (PUT method)', async ({ request }) => {
-    const response = await request.put(`https://backend.tallinn-learning.ee/login/student`, {
+  test('TL-12-4 Successful authorization, order creation and order deletion', async ({ request }) => {
+    const responseLogin = await request.post(`https://backend.tallinn-learning.ee/login/student`, {
       data: LoginDTO.createLoginWithCorrectData(),
     })
-    console.log(await response.text())
-    expect(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED)
+    expect(responseLogin.status()).toBe(StatusCodes.OK);
+
+    const responseCreateOrder = await request.post(`https://backend.tallinn-learning.ee/orders`, {
+      data: OrderDto.generateRandomOrderDto(),
+      headers: {
+        Authorization: "Bearer " + await responseLogin.text()
+      }
+    })
+    expect(responseCreateOrder.status()).toBe(StatusCodes.OK);
+    const createdOrder = OrderDto.serializeResponse(await responseCreateOrder.json())
+    expect(createdOrder.id).toBeDefined();
+    expect(createdOrder.id).toBeGreaterThan(0);
+
+    const responseDeleteOrder = await request.delete(`https://backend.tallinn-learning.ee/orders/${createdOrder.id}`, {
+      headers: {
+        Authorization: "Bearer " + await responseLogin.text()
+      }
+    })
+    expect(responseDeleteOrder.status()).toBe(StatusCodes.OK);
+    const requestedOrder = OrderDto.serializeResponse(await responseDeleteOrder.json());
+    expect(requestedOrder.status).toBeUndefined();
+    console.log(responseDeleteOrder.status());
+
   })
+
 })
