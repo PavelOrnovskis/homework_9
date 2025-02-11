eac15eb38d0323eaff6706dceb8a974a0feaed58
PavelOrnovskis <pavel.ornovskis@gmail.com>
2025-02-11 12:10:58 +0200

    2 different CIs and 2 test files

--- tests/empty-user.api.spec.ts
+++ tests/empty-user.api.spec.ts
@@ -1,7 +1,5 @@
 import { test, expect } from '@playwright/test'
 import { StatusCodes } from 'http-status-codes'
-import { UserDTO } from './DTO/UserDTO'
-import { APIRequestContext } from 'playwright-core'
 
 test('TL-14-5 return empty users through GET method', async ({ request }) => {
   const allUsersResponse = await request.get('http://localhost:3000/users')
